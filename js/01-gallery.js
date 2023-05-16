93befb1c755d1593633f6bfae1103a60083e7915
OlgaBilynska <olgabilinskaya@gmail.com>
2023-05-16 06:15:06 +0300

    task 1 stage 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = makeGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+
+function makeGalleryMarkup(images) {
+    return images.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+          `  ;
+    }).join('');
+};
+
+function onImageClick(event) {
+    const isImgEl = event.target.classList.contains('gallery__item');
+
+    if (!isImgEl) {
+        return;
+    }
+
+    const imgEl = event.target;
+    const imgGallery = imgEl.closest('.gallery');
+
+    console.log(imgGallery);
+}
+
+
+
+
