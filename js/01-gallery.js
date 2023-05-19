bf7eba7960f241be1bb1e1ff2d0f2481b6161aa6
OlgaBilynska <olgabilinskaya@gmail.com>
2023-05-19 16:39:35 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+// const instance = basicLightbox.create(`
+// 	<h1>Dynamic Content</h1>
+// 	<p>You can set the content of the lightbox with JS.</p>
+// `);
+
+
+// console.log(instance);
+// instance.show();
+
 
 const galleryContainer = document.querySelector('.gallery');
 const imagesMarkup = makeGalleryMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
 
+galleryContainer.addEventListener('click', onImageClick);
 
 function makeGalleryMarkup(images) {
     return images.map(({preview, original, description}) => {
         return `
         <li class="gallery__item">
-            <a class="gallery__link" href="large-image.jpg">
+            <a class="gallery__link" href="${original}">
                 <img
                     class="gallery__image"
                     src="${preview}"
@@ -27,14 +37,14 @@ function makeGalleryMarkup(images) {
 };
 
 function onImageClick(event) {
-    const isImgEl = event.target.classList.contains('gallery__item');
+    const isImgEl = event.target.classList.contains('gallery__image');
 
     if (!isImgEl) {
         return;
     }
 
     const imgEl = event.target;
-    const imgGallery = imgEl.closest('.gallery');
+    const imgGallery = imgEl.closest('.gallery__item');
 
     console.log(imgGallery);
 }
